739ea6bd45c7f5d4534a204da7132663da6c0a2a
ASUS <delenie1.3@gmail.com>
2021-10-15 12:00:34 +0500

    add class's revers & unittest

---
+++ revers.py
@@ -0,0 +1,15 @@
+class Reversed:
+    def __init__(self):
+        pass
+
+    def reversed(self, r_str):#метод смены направления букв
+        res = ''
+        for i in range(len(r_str)-1, -1, -1):#перебирает строку в обратном напровлении и записываает в res
+            res =+ r_str[i]
+        return res
+
+'''#для ручной проверки
+revers = Reversed()
+n = revers.reversed(input('Введите строку, для реверса: '))
+print(n)
+'''
