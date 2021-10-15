06d7615699359d7a4d6731557e5eeb168919ce20
ASUS <delenie1.3@gmail.com>
2021-10-15 12:00:53 +0500

    add class's revers & unittest

--- r_test.py
+++ r_test.py
@@ -1,1 +1,12 @@
 import unittest
+from revers import Reversed
+
+class TestReversed(unittest.TestCase):
+    def setUp(self):#создание экземпляра каласса при каждом запуске теста
+        self.revers = Reversed()
+
+    def test_reversed(self):
+        self.assertEqual(self.revers.reversed('Qwerty !'), '! ytrewQ')#проверяю ожидаемое значение
+
+if __name__ == '__main__':#запуск тестирования
+    unittest.main()
--- revers.py
+++ revers.py
@@ -5,7 +5,7 @@ class Reversed:
     def reversed(self, r_str):#метод смены направления букв
         res = ''
         for i in range(len(r_str)-1, -1, -1):#перебирает строку в обратном напровлении и записываает в res
-            res =+ r_str[i]
+            res += r_str[i]
         return res
 
 '''#для ручной проверки
